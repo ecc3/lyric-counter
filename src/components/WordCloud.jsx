0db7d35232b191875384650c8f172aad0daa4f58
Elizabeth <liz3.33zie@yahoo.co.uk>
2020-02-06 15:56:00 +0000

    added word cloud display

--- src/components/WordCloud.jsx
+++ src/components/WordCloud.jsx
@@ -1,10 +1,64 @@
 import React from "react";
+import ReactWordcloud from "react-wordcloud";
+import { Resizable } from "re-resizable";
 
-const WordCloud = () => {
+const resizeStyle = {
+  display: "flex",
+  alignItems: "stretch",
+  justifyContent: "center",
+  border: "solid 1px #ddd",
+  background: "#f0f0f0"
+};
+
+const options = {
+  colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"],
+  enableTooltip: true,
+  deterministic: false,
+  fontFamily: "impact",
+  fontSizes: [5, 60],
+  fontStyle: "normal",
+  fontWeight: "normal",
+  padding: 1,
+  rotations: 3,
+  rotationAngles: [0, 90],
+  scale: "sqrt",
+  spiral: "archimedean",
+  transitionDuration: 1000
+};
+
+const WordCloud = props => {
+  const { words } = props;
+
+  const wordsAndTotals = words.reduce((acc, val) => {
+    if (val.length > 3) {
+      acc[val] ? acc[val]++ : (acc[val] = 1);
+    }
+    return acc;
+  }, {});
+
+  const formattedWords = [];
+
+  for (const word in wordsAndTotals) {
+    if (wordsAndTotals[word] > 1) {
+      formattedWords.push({
+        text: word,
+        value: wordsAndTotals[word]
+      });
+    }
+  }
+
   return (
-    <div>
-      <p>Showing word cloud</p>
-    </div>
+    <Resizable
+      defaultSize={{
+        width: 600,
+        height: 300
+      }}
+      style={resizeStyle}
+    >
+      <div style={{ width: "100%", height: "100%" }}>
+        <ReactWordcloud options={options} words={formattedWords} />
+      </div>
+    </Resizable>
   );
 };
 
--- src/utils.js
+++ src/utils.js
@@ -1,51 +1,73 @@
-export const getArtistId = (artist, updateAverageWordCount) => {
+export const getArtistId = (artist, updateData) => {
   fetch(
     `http://musicbrainz.org/ws/2/artist/?query=artist:${artist}%20&fmt=json`
   )
     .then(response => response.json())
     .then(artistData => {
       const artistId = artistData.artists[0].id;
-      getSongsByArtistId(artistId, artist, updateAverageWordCount);
+      getSongsByArtistId(artistId, artist, updateData);
     });
 };
 
-const getSongsByArtistId = (artistId, artist, updateAverageWordCount) => {
+const getSongsByArtistId = (artistId, artist, updateData) => {
   fetch(`http://musicbrainz.org/ws/2/work?artist=${artistId}&fmt=json&limit=50`)
     .then(response => response.json())
     .then(worksData => {
-      getAverageWordcountForSongs(
-        worksData.works,
-        artist,
-        updateAverageWordCount
-      );
+      getAverageWordcountForSongs(worksData.works, artist, updateData);
     });
 };
 
-const getAverageWordcountForSongs = (songs, artist, updateAverageWordCount) => {
-  //instrumental songs?
-  //x3 repeats instead of lyrics?
+const getAverageWordcountForSongs = (songs, artist, updateData) => {
+  //average count unaffected by instumental songs, x3 repeats for example, singers named in lyrics
   let totalWordCount = 0;
+  let maxWordCount;
+  let minWordCount;
   let songCount = songs.length;
   let completedCount = 0;
+  let allSongWords = [];
   songs.forEach(song => {
     fetch(
       `https://orion.apiseeds.com/api/music/lyric/${artist}/${song.title}?apikey=8bNBudkPyx6tcUQJ2dRUtPlUF6bwnRGPlrvnIZ4m5ySvJIziuLAP8o4TJ8uST2A7`
     )
       .then(response => response.json())
       .then(lyricData => {
-        const songWordCount = lyricData.result.track.text.split(/\s/).length;
+        const songWords = lyricData.result.track.text.split(/\s/);
+        allSongWords = allSongWords.concat(songWords);
+
+        const songWordCount = songWords.length;
         totalWordCount += songWordCount;
         completedCount++;
+
+        if (!minWordCount) {
+          minWordCount = songWordCount;
+          maxWordCount = songWordCount;
+        }
+        if (songWordCount < minWordCount) {
+          minWordCount = songWordCount;
+        }
+        if (songWordCount > maxWordCount) {
+          maxWordCount = songWordCount;
+        }
         if (completedCount === songCount) {
           const averageWordCount = (totalWordCount / songCount).toFixed(0);
-          updateAverageWordCount(averageWordCount);
+          updateData(
+            averageWordCount,
+            minWordCount,
+            maxWordCount,
+            allSongWords
+          );
         }
       })
       .catch(err => {
         songCount--;
         if (completedCount === songCount) {
           const averageWordCount = (totalWordCount / songCount).toFixed(0);
-          updateAverageWordCount(averageWordCount);
+          updateData(
+            averageWordCount,
+            minWordCount,
+            maxWordCount,
+            allSongWords
+          );
         }
       });
   });
