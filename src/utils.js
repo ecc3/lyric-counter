d2c812a675c47f51cadc2adeb5cd34bac6c8bf2e
Elizabeth <liz3.33zie@yahoo.co.uk>
2020-02-06 11:25:28 +0000

    fetch artist ID

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,12 @@
-export const getArtistSongs = () => {
-  console.log("get artist songs");
+export const getArtistSongs = artist => {
+  fetch(
+    `http://musicbrainz.org/ws/2/artist/?query=artist:${artist}%20&fmt=json`
+  )
+    .then(response => response.json())
+    .then(artistData => {
+      return artistData.artists[0].id;
+    })
+    .then(artistID => {
+      console.log(artistID);
+    });
 };
