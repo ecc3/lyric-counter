184f89e64f20a9f3c878a6e633986a395aa7deeb
Elizabeth <liz3.33zie@yahoo.co.uk>
2020-02-06 18:21:00 +0000

    Updates

---
+++ precache-manifest.3d983b9f6f8bcbfd9911ce2a8e83a8f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5998f0daf9979e1ae0b4b55d31042ca0",
+    "url": "/lyric-counter/index.html"
+  },
+  {
+    "revision": "4dbdd397499c95b18f65",
+    "url": "/lyric-counter/static/css/main.767d560b.chunk.css"
+  },
+  {
+    "revision": "d02ac8b6cd38631c21a7",
+    "url": "/lyric-counter/static/js/2.ed23650d.chunk.js"
+  },
+  {
+    "revision": "334b4b987003d90b9c311ba0ca8dd3aa",
+    "url": "/lyric-counter/static/js/2.ed23650d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dbdd397499c95b18f65",
+    "url": "/lyric-counter/static/js/main.d501e9e0.chunk.js"
+  },
+  {
+    "revision": "db3de9f77443f6c86d93",
+    "url": "/lyric-counter/static/js/runtime-main.f226f28d.js"
+  },
+  {
+    "revision": "f9ba31c75eddcda470f81c04de8087c5",
+    "url": "/lyric-counter/static/media/leaves-336694_1920.f9ba31c7.jpg"
+  }
+]);
